9d37aca8873f47c9bd4db9993097f5071dcd503e
Naveen Chandra <the.naveenchandra@gmail.com>
2023-05-19 16:55:07 +0530

    exports

--- lib/components/components.dart
+++ lib/components/components.dart
@@ -1,3 +1,6 @@
 export './variables.dart';
 export './blank_pixel.dart';
 export './snake_pixel.dart';
+export './food_pixel.dart';
+export './head_snake_pixel.dart';
+export './tail_snake_pixel.dart';
